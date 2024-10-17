99d65176e01d577f97a134955f0a0ef8c76b928b
auxano1003 <temitayo1003@gmail.com>
2024-10-18 00:58:04 +0100

    the correction on the sign up on the firebase

--- P.S project/Dashboard.js
+++ P.S project/Dashboard.js
@@ -1,87 +1,90 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getDatabase, onValue, ref } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js';
+import {
+  getDatabase,
+  onValue,
+  ref,
+} from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBs8xzZ_NImJb45cGgURPSfsYP42hn5yNw",
-    authDomain: "sqi-level-two-project.firebaseapp.com",
-    databaseURL: "https://sqi-level-two-project-default-rtdb.firebaseio.com",
-    projectId: "sqi-level-two-project",
-    storageBucket: "sqi-level-two-project.appspot.com",
-    messagingSenderId: "543903052576",
-    appId: "1:543903052576:web:e276b5d2b7175fcabddd45",
-    measurementId: "G-XSLF84G9DK"
+  apiKey: 'AIzaSyBs8xzZ_NImJb45cGgURPSfsYP42hn5yNw',
+  authDomain: 'sqi-level-two-project.firebaseapp.com',
+  databaseURL: 'https://sqi-level-two-project-default-rtdb.firebaseio.com',
+  projectId: 'sqi-level-two-project',
+  storageBucket: 'sqi-level-two-project.appspot.com',
+  messagingSenderId: '543903052576',
+  appId: '1:543903052576:web:e276b5d2b7175fcabddd45',
+  measurementId: 'G-XSLF84G9DK',
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-let userDisplay = document.getElementById("userDisplay");
-let imgOne = document.getElementById("imgOne");
-let settIngs = document.getElementById("settIngs");
+let userDisplay = document.getElementById('userDisplay');
+let imgOne = document.getElementById('imgOne');
+let settIngs = document.getElementById('settIngs');
 
-let user = JSON.parse(localStorage.getItem("UserInformation"));
+let user = JSON.parse(localStorage.getItem('UserInformation'));
 // console.log(user)
+
 const lastName = user.lastName;
 userDisplay.innerHTML = `Hi, ${user.firstName}`;
 
-const userRef = ref(db, `UserDetails/${lastName}`)
+const userRef = ref(db, `UserDetails/${lastName}`);
 onValue(userRef, (userInfo) => {
-    const userdetails = userInfo.val()
-    localStorage.setItem("UserInformation", JSON.stringify(userdetails))
-    let balAnce = document.getElementById("balAnce");
-    // console.log(userdetails)
-    balAnce.innerHTML = userdetails.acctBalance; 
-})
+  const userdetails = userInfo.val();
+  localStorage.setItem('UserInformation', JSON.stringify(userdetails));
+  let balAnce = document.getElementById('balAnce');
+  // console.log(userdetails)
+  balAnce.innerHTML = userdetails.acctBalance;
+});
 
-let userInt = JSON.parse(localStorage.getItem("UserInformation"));
+let userInt = JSON.parse(localStorage.getItem('UserInformation'));
 
-imgOne.addEventListener('click', function() {
-    window.location.href = "Profile.html"
-})
+imgOne.addEventListener('click', function () {
+  window.location.href = 'Profile.html';
+});
 
 settIngs.addEventListener('click', function () {
-    window.location.href = "Settings.html"
-})
+  window.location.href = 'Settings.html';
+});
 
 window.addEventListener('load', function () {
-    showPass.style.display = "none"
-})
+  showPass.style.display = 'none';
+});
 
-
-let hidePass = document.getElementById("hidePass");
-let showPass = document.getElementById("showPass");
-
+let hidePass = document.getElementById('hidePass');
+let showPass = document.getElementById('showPass');
 
 hidePass.addEventListener('click', function () {
-    balAnce.innerHTML = "*******"
-    hidePass.style.display = "none"
-    showPass.style.display = "inline"
-})
+  balAnce.innerHTML = '*******';
+  hidePass.style.display = 'none';
+  showPass.style.display = 'inline';
+});
 
 showPass.addEventListener('click', function () {
-    balAnce.innerHTML = userInt.acctBalance
-    hidePass.style.display = "inline"
-    showPass.style.display = "none"
-})
+  balAnce.innerHTML = userInt.acctBalance;
+  hidePass.style.display = 'inline';
+  showPass.style.display = 'none';
+});
 
 //Functions
 //Transfer Funds
 
-let transFer = document.getElementById("transFer")
+let transFer = document.getElementById('transFer');
 transFer.addEventListener('click', function () {
-    window.location.href = "Transfer.html"
-})
+  window.location.href = 'Transfer.html';
+});
 
 //Airtime
-let airTime = document.getElementById("airTime")
+let airTime = document.getElementById('airTime');
 
 airTime.addEventListener('click', function () {
-    window.location.href = "Airtime.html"
-})
+  window.location.href = 'Airtime.html';
+});
 
 //Transaction History
-let tranHistory = document.getElementById("tranHistory")
+let tranHistory = document.getElementById('tranHistory');
 
 tranHistory.addEventListener('click', function () {
-    window.location.href = "History.html"
-})
+  window.location.href = 'History.html';
+});
--- P.S project/Sign Up.js
+++ P.S project/Sign Up.js
@@ -1,69 +1,76 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBs8xzZ_NImJb45cGgURPSfsYP42hn5yNw",
-    authDomain: "sqi-level-two-project.firebaseapp.com",
-    databaseURL: "https://sqi-level-two-project-default-rtdb.firebaseio.com",
-    projectId: "sqi-level-two-project",
-    storageBucket: "sqi-level-two-project.appspot.com",
-    messagingSenderId: "543903052576",
-    appId: "1:543903052576:web:e276b5d2b7175fcabddd45",
-    measurementId: "G-XSLF84G9DK"
+  apiKey: 'AIzaSyBs8xzZ_NImJb45cGgURPSfsYP42hn5yNw',
+  authDomain: 'sqi-level-two-project.firebaseapp.com',
+  databaseURL: 'https://sqi-level-two-project-default-rtdb.firebaseio.com',
+  projectId: 'sqi-level-two-project',
+  storageBucket: 'sqi-level-two-project.appspot.com',
+  messagingSenderId: '543903052576',
+  appId: '1:543903052576:web:e276b5d2b7175fcabddd45',
+  measurementId: 'G-XSLF84G9DK',
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-let createAccount = document.getElementById("createAccount");
 
+let createAccount = document.getElementById('createAccount');
+let deleteAccount = document.getElementById('delete');
 
-createAccount.addEventListener("click", function (ev) {
-    ev.preventDefault();
+//createAccount.addEventListener("click", function(edit))
 
-    let firstName = document.getElementById("firstName");
-    let lastName = document.getElementById("lastName");
-    let emailAddress = document.getElementById("emailAddress");
-    let passWord = document.getElementById("passWord");
-    let confirmPassword = document.getElementById("confirmPassword");
+createAccount.addEventListener('click', function (ev) {
+  ev.preventDefault();
 
-    let UserInformation = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: emailAddress.value,
-        acctNumber: ""
-    };
+  let firstName = document.getElementById('firstName');
+  let lastName = document.getElementById('lastName');
+  let emailAddress = document.getElementById('emailAddress');
+  let passWord = document.getElementById('passWord');
+  let confirmPassword = document.getElementById('confirmPassword');
 
-    if (passWord.value === "") {
-    return alert("Password invalid");
-    } else if (passWord.value !== confirmPassword.value) {
-        return alert("Password does not match");
-    } else if (passWord.value.length < 8) {
-        return alert("Password must be a minimum of 8 characters");
-    }
+  let UserInformation = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: emailAddress.value,
+    password: password.value,
+    acctNumber: '',
+  };
 
-    // console.log("UserInformation : ", UserInformation);
+  if (passWord.value === '') {
+    return alert('Password invalid');
+  } else if (passWord.value !== confirmPassword.value) {
+    return alert('Password does not match');
+  } else if (passWord.value.length < 8) {
+    return alert('Password must be a minimum of 8 characters');
+  }
 
-    const email = document.getElementById("emailAddress").value;
-    const password = document.getElementById("passWord").value;
+  // console.log("UserInformation : ", UserInformation);
 
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            // console.log("User Info : ", user);
-            alert("User signed up successfully");
-            window.location.href = "Loading.html";
-            // console.log(UserInformation);
-            localStorage.setItem("UserInformation", JSON.stringify(UserInformation));
-        })
-        .catch((error) => {
-            if (error.code === "auth/email-already-in-use") {
-               return alert("Email already in use");
-            } else if (error.code === "auth/invalid-email") {
-               return alert("Invalid Email");
-            } else if (error.code === "auth/weak-password") {
-               return alert("Weak Password");
-            } else {
-               return alert(error.message);
-            }
-        });
-})
+  const email = document.getElementById('emailAddress').value;
+  const password = document.getElementById('passWord').value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      // console.log("User Info : ", user);
+      alert('User signed up successfully');
+      window.location.href = 'Loading.html';
+      // console.log(UserInformation);
+      localStorage.setItem('UserInformation', JSON.stringify(UserInformation));
+    })
+    .catch((error) => {
+      if (error.code === 'auth/email-already-in-use') {
+        return alert('Email already in use');
+      } else if (error.code === 'auth/invalid-email') {
+        return alert('Invalid Email');
+      } else if (error.code === 'auth/weak-password') {
+        return alert('Weak Password');
+      } else {
+        return alert(error.message);
+      }
+    });
+});
